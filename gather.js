fb3c2555e27689f63913d84c53d6a4f602ee713d
Colin <colin.diesh@gmail.com>
2024-11-21 20:40:00 -0500

    Wow

--- gather.js
+++ gather.js
@@ -1,22 +1,24 @@
 import fs from 'fs'
-const f = fs.readdirSync('json')
-console.log(['program', 'size', 'time', 'min', 'max'].join('\t'))
 console.log(
-  f
-    .filter(f => f !== 'README.md')
-    .map(file => {
-      console.log
-      const text = fs.readFileSync(`json/${file}`, 'utf8')
-      const res = JSON.parse(text)
-      const e = res.results[0]
-      console.log({ res: res.results[0] })
-      return [
-        file.split('-')[0],
-        file.split('-')[1].split('_')[0],
-        e.mean,
-        e.mean - e.stddev,
-        e.mean + e.stddev,
-      ].join('\t')
-    })
-    .join('\n'),
+  [
+    ['program', 'size', 'time', 'mean', 'sd'].join('\t'),
+    ...fs
+      .readdirSync('json')
+      .filter(f => f !== 'README.md')
+      .map(file => {
+        const text = fs.readFileSync(`json/${file}`, 'utf8')
+        const res = JSON.parse(text)
+        const e = res.results[0]
+        return e.times.map(time =>
+          [
+            file.split('-')[0],
+            file.split('-')[1].split('_')[0],
+            time,
+            e.mean,
+            e.stddev,
+          ].join('\t'),
+        )
+      })
+      .flat(),
+  ].join('\n'),
 )
--- plot.R
+++ plot.R
@@ -1,11 +1,12 @@
 library(ggplot2)
 
 df <- read.table("out.tsv", header = TRUE)
-
-ggplot(df, aes(x = size, y = time, color = program)) +
-  geom_line() +
+x <- ggplot(df, aes(x = size, y = time, color = program)) +
+  geom_line(stat = "summary") +
+  geom_errorbar(width = 0.2, stat = "summary") +
+  geom_jitter(size = 0.5, position = position_jitter(width = 0.1, seed = 42)) +
   scale_x_continuous(trans = "log2") +
   ylab("time (s)") +
   xlab("MSA size NxN") +
   ggtitle("Time to render MSA")
-ggsave("img/1.png")
+ggsave("img/1.png", x, width = 7, height = 4)
